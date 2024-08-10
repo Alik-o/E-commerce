04cf44f7c4984f2d5063a0df35e5f100526a4f22
Oleg Navoev <navoev.oleg@gmail.com>
2024-08-10 21:12:17 +0500

    Реализованы функции чтения и создания объектов из фйла json в модуле utils

---
+++ src/utils.py
@@ -0,0 +1,27 @@
+import json
+import os
+
+from config import DATA_DIR
+from src.category import Category
+from src.product import Product
+
+
+def load_json(path: str) -> list[dict]:
+    """Загружает json из файла"""
+    with open(os.path.join(DATA_DIR, path), "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def create_object_from_json(data: list) -> list:
+    """Создает объекты из словаря"""
+    result = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(product["name"], product["description"], product["price"], product["quantity"]))
+        result.append(Category(category["name"], category["description"], products))
+
+    return result
+
+
+print(load_json("products.json"))
