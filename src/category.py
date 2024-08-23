97b0b2b06fcfaad9195346ea3234792157826904
OlegNavoev <navoev.oleg@gmail.com>
2024-08-23 22:38:11 +0500

    добавлены приватные атрибуты и методы в классе Category

--- src/category.py
+++ src/category.py
@@ -1,16 +1,35 @@
 class Category:
     name: str
     description: str
-    products: list
-    category_count: int
-    product_count: int
+    __products: list
+    _category_count: int
+    _product_count: int
 
-    category_count = 0
-    product_count = 0
+    _category_count = 0
+    _product_count = 0
 
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
-        Category.category_count += 1
-        Category.product_count += len(self.products)
+        self.__products = products
+        Category._category_count += 1
+        Category._product_count += len(self.__products)
+
+    def add_product(self, product):
+        self.__products.append(product)
+        self._product_count += 1
+
+    @property
+    def products(self):
+        products = ""
+        for product in self.__products:
+            products += f"{product.name}, {product.price} руб. Остаток: {product.quantity}\n"
+        return products
+
+    @classmethod
+    def get_category_count(cls):
+        return cls._category_count
+
+    @classmethod
+    def get_product_count(cls):
+        return cls._product_count
