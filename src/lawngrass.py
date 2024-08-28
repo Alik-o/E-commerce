69d21bc4664099dd5ae6eafd50a996e75e9bbd71
OlegNavoev <navoev.oleg@gmail.com>
2024-08-28 22:43:18 +0500

    добавлен новый класс lawngrass

---
+++ src/lawngrass.py
@@ -0,0 +1,14 @@
+from src.product import Product
+
+
+class LawnGrass(Product):
+
+    def __init__(self, name, description, price, quantity, country, germination_period, color=None):
+        super().__init__(name, description, price, quantity, color)
+        self.country = country
+        self.germination_period = germination_period
+
+    def __add__(self, other):
+        if type(other) is LawnGrass:
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError
