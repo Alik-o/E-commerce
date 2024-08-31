0ba3afa4d0a57e390ff2d46927d2535533037f0d
OlegNavoev <navoev.oleg@gmail.com>
2024-08-31 21:51:13 +0500

    в Product добавлена проверка количества товара

--- src/product.py
+++ src/product.py
@@ -12,6 +12,8 @@ class Product(BaseProduct, PrintMixin):
         self.name = name
         self.description = description
         self.__price = price
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         self.quantity = quantity
         self.color = color
         super().__init__()
